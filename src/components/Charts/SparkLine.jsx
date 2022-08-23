f3bff30a13f50d90508df17abbb71b4439e0cf94
amanVar06 <98205162+amanVar06@users.noreply.github.com>
2022-08-23 22:38:09 +0530

    Started Revenue update card in homepage but error in Sparkline component

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  SparklineTooltip,
+  SparklineComponent,
+  Inject,
+} from "@syncfusion/ej2-react-charts";
 
-const SparkLine = () => {
-  return <div>SparkLine</div>;
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName="x"
+      yName="y"
+      type={type}
+    >
+      <Inject services={{ SparklineTooltip }} />
+    </SparklineComponent>
+  );
 };
 
 export default SparkLine;
